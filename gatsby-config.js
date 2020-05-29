562c56a5015d2b90e6d2144903f03f1b933eedb2
mesuara <mesuara@gmail.com>
2020-05-28 23:50:02 -0400

    aded google fonts

--- gatsby-config.js
+++ gatsby-config.js
@@ -40,7 +40,17 @@ module.exports = {
           },
         ],
       }
-    }
+    },
+    {
+      resolve: `gatsby-plugin-google-fonts`,
+      options: {
+        fonts: [
+          `roboto mono`,
+          `muli\:400,400i,700,700i`,
+        ],
+        display: "swap",
+      },
+    },
   ],
 
 }
