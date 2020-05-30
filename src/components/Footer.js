c6c1903eefb8e8433806e5d61d2f778e9c542df4
mesuara <mesuara@gmail.com>
2020-05-29 22:49:17 -0400

    added footer

---
+++ src/components/Footer.js
@@ -0,0 +1,43 @@
+import React from 'react'
+import {useStaticQuery, graphql} from "gatsby"
+import {FooterWrapper, FooterSocialWrapper, FooterSocialIcons} from "../elements"
+
+export const Footer =() =>{
+    const data = useStaticQuery(graphql`
+        query {
+            facebook: file(relativePath: {eq: "facebooklogo.png"}){
+                publicURL
+            }
+            instagram: file(relativePath: {eq: "instagramlogo.png"}){
+                publicURL
+            }
+            twitter: file(relativePath: {eq: "twitterlogo.png"}){
+                publicURL
+            }
+            linkedin: file(relativePath: {eq: "linkedinlogo.png"}){
+                publicURL
+            }
+        }
+    `)
+    return (
+        <FooterWrapper>
+            <FooterSocialWrapper>
+                <FooterSocialIcons>
+                    <a href="https://facebook.com" target="_blan" rel="noopener noreferrer">
+                        <img src ={data.facebook.publicURL} alt="Facebook Logo"/>
+                    </a>
+                    <a href="https://instagram.com" target="_blan" rel="noopener noreferrer">
+                        <img src ={data.instagram.publicURL} alt="Instagram Logo"/>
+                    </a>
+                    <a href="https://twitter.com" target="_blan" rel="noopener noreferrer">
+                        <img src ={data.twitter.publicURL} alt="Twitter Logo"/>
+                    </a>
+                    <a href="https://linkedin.com" target="_blan" rel="noopener noreferrer">
+                        <img src ={data.linkedin.publicURL} alt="Linkedin Logo"/>
+                    </a>
+                </FooterSocialIcons>
+                <p>2020 Company. All right reseved.</p>
+            </FooterSocialWrapper>
+        </FooterWrapper>
+    )
+}
---
+++ src/elements/FooterElements.js
@@ -0,0 +1,40 @@
+import styled from "styled-components"
+
+export const FooterWrapper = styled.footer`
+    grid-column: 2/ span 12;
+    min-height: 11.25rem;
+    display: flex;
+    justify-content: center;
+    alignt-items: flex-start;
+
+    @medias ${props => props.theme.breakpoints.tablet}{
+        grid-column: 2 / span 6;
+    }
+`
+
+export const FooterSocialWrapper = styled.div`
+    display: flex;
+    flex-wrap: wrap;
+    aling-items: center;
+    text-align: center;
+
+    & p {
+        text-align:center;
+        flex: 0 0 100%;
+    }
+`
+
+export const FooterSocialIcons = styled.div`
+    flex: 0 0 100%;
+    margin-bottom: 2rem;
+
+    & img {
+        height: 22px;
+        padding: 0 1rem;
+        transition: filter 0.3s ease;
+    }
+
+    & img:hover, img:focus {
+        filter: brightness(50%)
+    }
+`
