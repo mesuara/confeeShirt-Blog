0b450097e5a0b2630aaea225546fe3bbb189dbdb
mesuara <mesuara@gmail.com>
2020-05-30 19:26:53 -0400

    created typography

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,6 +1,7 @@
 import React from 'react'
 import {useStaticQuery, graphql} from "gatsby"
-import {FooterWrapper, FooterSocialWrapper, FooterSocialIcons} from "../elements"
+import {FooterWrapper, FooterSocialWrapper, FooterSocialIcons, P} from "../elements"
+
 
 export const Footer =() =>{
     const data = useStaticQuery(graphql`
@@ -36,7 +37,7 @@ export const Footer =() =>{
                         <img src ={data.linkedin.publicURL} alt="Linkedin Logo"/>
                     </a>
                 </FooterSocialIcons>
-                <p>2020 Company. All right reseved.</p>
+                <P size="xSmall" color="dark3" >2020 Company. All right reseved.</P>
             </FooterSocialWrapper>
         </FooterWrapper>
     )
--- src/elements/ContentElements.js
+++ src/elements/ContentElements.js
@@ -4,16 +4,16 @@ export const ContentWrapper = styled.main`
     grid-column: 4 / span 8;
     grid-row: 3 / span 5;
     background-color: ${props => props.theme.colors.light2};
-    padding: ${props => `${props.theme.spacings.xLarge} ${porps.theme.spacings.xxLarge}`};
+    padding: ${props => `${props.theme.spacings.xLarge} ${props.theme.spacings.xxLarge}`};
     box-shadow: ${props=> props.theme.shadows.shadow1};
     z-index : 10;
 
-    @media {props => props.theme.breakpoints.tablet}{
+    @media ${props => props.theme.breakpoints.tablet}{
         grid-column: 2 / span 6;
 
     }
-    @media {props => props.theme.breakpoints.mobile}{
-        padding: ${props => `${props.theme.spacings.medium} ${porps.theme.spacings.large}`};
+    @media ${props => props.theme.breakpoints.mobile}{
+        padding: ${props => `${props.theme.spacings.medium} ${props.theme.spacings.large}`};
 
         
     }
