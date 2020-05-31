e505162be72771a1daed5c647926708807413c9c
mesuara <mesuara@gmail.com>
2020-05-30 20:32:59 -0400

    added card components

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,10 +1,13 @@
 import React from "react"
-import { Container, FeatureImage } from "../components"
+import { Container, FeatureImage, Content, ContentCard } from "../components"
 
 export const IndexPage = () =>{
   return (
     <Container>
       <FeatureImage/>
+      <Content>
+        <ContentCard date="2020" title="Palidhje" exerpt="Tu testu diqka qetu, spo di" slug="/test-blog"/>
+      </Content>
     </Container>
 
 
